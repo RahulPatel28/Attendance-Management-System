667d92ab29c885d2dbea541ec69ed6d040d2f6a7
RahulPatel28 <gurjarpatel118@gmail.com>
2025-04-17 12:32:12 +0530

    Update Index.js

--- Backend/Index.js
+++ Backend/Index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend URL
+    origin: "https://attendance-management-system-frontend.onrender.com", // your frontend URL
     credentials: true, // to allow cookies, tokens, headers, etc.
   })
 );
